5cbc8930014135eb8d91e4f94a488a2925b15329
adkwn1 <a.d.kwn@proton.me>
2025-12-03 21:27:06 -0500

    Day 3 solution.

--- 2025/d_3/day_3.py
+++ 2025/d_3/day_3.py
@@ -2,55 +2,38 @@
 # edit the line above to the appropriate path if required
 
 
-def solve1(input:str):
+def first_largest_idx(array:list):
+    max_num = 0
+    idx = 0
+
+    for i, n in enumerate(array):
+        if int(n) > max_num:
+            max_num = int(n)
+            idx = i
+
+    return idx
+
+
+def solve(input:str, size:int):
     count = 0
     with open(input, "r") as file:
         for line in file:
             bank = line.strip()
-            first = bank[0]
-            second = "0"
-            for i in range(1, len(bank)):
-                if int(bank[i]) > int(first) and (i < len(bank)-1):
-                    first = bank[i]
-                    second = bank[i+1]
-                elif int(bank[i]) >= int(second):
-                    second = bank[i]                    
-            count += int(first + second)
-    return count
+            result = []
+            idx = first_largest_idx(bank)
 
-
-def solve2(input:str):
-    count = 0
-    with open(input, "r") as file:
-        for line in file:
-            bank = line.strip()
-            min_num = 100
-            min_idx = 0
-            array = [int(b) for b in bank[:12]]
-    
-            for idx, n in enumerate(array):
-                if n < min_num:
-                    min_num = n
-                    min_idx = idx
-    
-            for i in range(12, len(bank)):
-                if array[0] < array[1]:
-                    array.pop(0)
-                elif int(bank[i]) >= min_num:
-                    array.pop(min_idx)
-                array.append(int(bank[i]))
-                min_num = min(array)
-                min_idx = array.index(min_num)
-            #print(array)
-            count += int("".join([str(s) for s in array]))            
+            while len(result) < size:
+                if len(bank[idx:]) >= (size - len(result)):
+                    result.append(bank[idx])
+                    bank = bank[idx+1:]
+                    idx = first_largest_idx(bank)
+                else:
+                    idx = first_largest_idx(bank[:idx])
+            
+            count += int("".join(result))
     
     return count
 
                             
 if __name__ == "__main__":
-    print(solve1("./input.txt"), solve2("./input.txt"))
-   # print(solve2("./test.txt"))
-
-# 150751623305028 too low
-# 155590471707149 too low
-# 783426553274286915064646097389328607328213 too high
+   print(solve("./input.txt", 2), solve("./input.txt", 12))
